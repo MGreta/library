483ea47745f0b35d66bbb44f3c20c7c8979df76d
GretaM <gretamerkeviciute@gmail.com>
2017-04-09 23:07:03 +0300

    Admin users blade update.

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -1,8 +1,14 @@
-@extends('layouts.admin')
+@extends('welcome')
 
 @section('content')
 
-
+@if($errors->any())
+    <script>
+        $(function() {
+            $('#userEdit{{session('wrong_id')}}').modal('show');
+        });
+    </script>
+@endif
 <div class="panel panel-primary">
     <div class="panel-heading">Users</div>
     <div class="panel-body">
@@ -32,7 +38,64 @@
                                     @endforeach
                                 </td>
                                 <td>
-                                    <a class="btn btn-default btn-xs" href="{{ url('/user/' . $users[$i]->id . '/edit') }}"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></a>
+                                    <a class="btn btn-default btn-xs" data-toggle="modal" data-target="#userEdit{{ $users[$i]->id }}"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></a>
+                                    <div class="modal fade" id="userEdit{{ $users[$i]->id }}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                                        <div class="modal-dialog" role="document">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                                    <h4 class="modal-title" id="myModalLabel">{{$users[$i]->name}} {{$users[$i]->last_name}}</h4>
+                                                </div>
+                                                <div class="modal-body">
+                                                @include ('partials.notice')
+                                                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/user/' . $users[$i]->id . '/edit') }}">
+                                                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+                                                        <div class="form-group">
+                                                            <label class="col-sm-2 control-label" for="title">Name</label>
+                                                            <div class="col-sm-10">
+                                                                <input type="text" class="form-control" id="name" name="name" value="{{ old('name', $users[$i]->name) }}">
+                                                            </div>
+                                                        </div>
+
+                                                        <div class="form-group">
+                                                            <label class="col-sm-2 control-label" for="author">Last Name</label>
+                                                            <div class="col-sm-10">
+                                                                <input type="text" class="form-control" id="last_name" name="last_name" value="{{ old('last_name', $users[$i]->last_name) }}">
+                                                            </div>
+                                                        </div>
+
+                                                        <div class="form-group">
+                                                            <label class="col-sm-2 control-label" for="isbn">Class</label>
+                                                            <div class="col-sm-10">
+                                                                <input type="text" class="form-control" id="class" name="class" value="{{ old('class', $users[$i]->class) }}">
+                                                            </div>
+                                                        </div>
+
+                                                        <div class="form-group">
+                                                            <label class="col-sm-2 control-label" for="date">Email</label>
+                                                            <div class="col-sm-10">
+                                                                <input type="text" class="form-control" id="email" name="email" value="{{ old('email', $users[$i]->email) }}">
+                                                            </div>
+                                                        </div>
+
+                                                        <div class="form-group">
+                                                            <div class="col-md-6 col-md-offset-4">
+                                                                <button type="submit" class="btn btn-primary">
+                                                                    Save Changes
+                                                                </button>
+                                                            </div>
+                                                        </div>
+                                                    </form>
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                                </div>
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- <a class="btn btn-default btn-xs" href="{{ url('/user/' . $users[$i]->id . '/edit') }}"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></a> -->
                                     <a class="btn btn-default btn-xs" href="#" data-toggle="modal" data-target="#users-delete-modal" data-users-email="{{ $users[$i]->email }}" data-users-id="{{ $users[$i]->id }}"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></a>
                                 </td>
                             <td>
