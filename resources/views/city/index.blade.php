e13f748b06e0e211cc668e3342ad457be176cb86
GretaM <gretamerkeviciute@gmail.com>
2017-04-09 23:08:50 +0300

    City blade update.

--- resources/views/city/index.blade.php
+++ resources/views/city/index.blade.php
@@ -2,7 +2,13 @@
 
 @section('content')
 
-
+@if($errors->any())
+    <script>
+        $(function() {
+            $('#editCity{{session('wrong_id')}}').modal('show');
+        });
+    </script>
+@endif
 <div class="">
 @if (Auth::user())
     @if (Auth::user()->hasRole("admin"))
@@ -51,12 +57,48 @@
                                     <tr>
                                         <th>{{ $i+1 }}</th>
                                             <td>{{ $city[$i]->city }}</td>
-                                            <td>{{ get_books_by_cities($city[$i]->id) }}</td>
+                                            <td>{{-- get_books_by_cities($city[$i]->id) --}}</td>
                                             @if (Auth::user())
                                             @if (Auth::user()->hasRole("admin"))
                                             <td>
-                                                <a class="btn btn-default btn-xs" href="{{ url('/city/' . $city[$i]->id . '/city') }}"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></a>
-                                                <a class="btn btn-default btn-xs" href="#" data-toggle="modal" data-target="#languages-delete-modal" data-languages="{{ $types[$i]->type }}" data-languages-id="{{ $types[$i]->id }}"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></a>
+                                                <a class="btn btn-default btn-xs" data-toggle="modal" data-target="#editCity{{ $city[$i]->id }}"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></a>
+                                                <div class="modal fade" id="editCity{{ $city[$i]->id }}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                                                    <div class="modal-dialog" role="document">
+                                                        <div class="modal-content">
+                                                            <div class="modal-header">
+                                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                                                <h4 class="modal-title" id="myModalLabel">{{$city[$i]->city}}</h4>
+                                                            </div>
+                                                            <div class="modal-body">
+                                                            @include ('partials.notice')
+                                                                <form class="form-horizontal" role="form" method="POST" action="{{ url('/city/' . $city[$i]->id . '/edit') }}">
+                                                                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+                                                                    <div class="form-group">
+                                                                        <label class="col-sm-2 control-label" for="city">City</label>
+                                                                        <div class="col-sm-10">
+                                                                            <input type="text" class="form-control" id="city" name="city" value="{{ old('city', $city[$i]->city) }}">
+                                                                        </div>
+                                                                    </div>
+
+                                                                    <div class="form-group">
+                                                                        <div class="col-md-6 col-md-offset-4">
+                                                                            <button type="submit" class="btn btn-primary">
+                                                                                Save Changes
+                                                                            </button>
+                                                                        </div>
+                                                                    </div>
+                                                                </form>
+                                                            </div>
+                                                            <div class="modal-footer">
+                                                                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                                            </div>
+
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <!-- <a class="btn btn-default btn-xs" href="{{ url('/city/' . $city[$i]->id . '/city') }}"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></a> -->
+                                                <a class="btn btn-default btn-xs" href="{{ url('/city/' . $city[$i]->id . '/destroy') }}"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></a>
                                             </td>
                                             @endif
                                             @endif
