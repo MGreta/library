dc85f53f133aa9b8f2b54fd0b9b9dee80baa4479
GretaM <gretamerkeviciute@gmail.com>
2017-04-09 22:58:46 +0300

    Book ratings update.

--- app/Http/Controllers/BookRatingsController.php
+++ app/Http/Controllers/BookRatingsController.php
@@ -17,25 +17,47 @@ class BookRatingsController extends Controller
     public function rate(Request $request, $id)
     {
     	$user_id = Auth::user()->id;
-    	$validator =  Validator::make($request->all(), [
-            'book_rating' => 'required|max:255'
-        ]);
+        if ($rating = BookRatings::where('book_id', $id)->where('user_id', $user_id)->value('id'))
+        {
+            $validator =  Validator::make($request->all(), [
+                'book_rating' => 'required|max:255'
+            ]);
 
-        if ($validator->fails()) {
+            if ($validator->fails()) {
+                return redirect()->back()->withErrors(['error' => 'Klaida. Neleistinas veiksmas.']);
+            }
 
-            return redirect()->back()->withErrors($validator)->withInput();
+            if ($rating = BookRatings::find($rating)) {
+                $rating->book_rating = $request->input('book_rating');
+                $response = $rating->save();
+                if ($response) {
+                    return redirect()->back()->with(['message' => 'Knyga atnaujintas.']);
+                }
+                return redirect()->back()->with('errors', new MessageBag(['Something went wrong while adding new genre. Please try again.']));
+            }
         }
+        else {
+            $validator =  Validator::make($request->all(), [
+                'book_rating' => 'required|max:255'
+            ]);
 
-        $rating = BookRatings::create([
-            'book_rating' => $request->input('book_rating'),
-            'book_id' => $id,
-            'user_id' => $user_id
-        ]);
-        if ($rating) {
+            if ($validator->fails()) {
 
-            return redirect()->back()->with('status', 'Genre created successfully.');
+                return redirect()->back()->withErrors($validator)->withInput();
+            }
+
+            $rating = BookRatings::create([
+                'book_rating' => $request->input('book_rating'),
+                'book_id' => $id,
+                'user_id' => $user_id
+            ]);
+            if ($rating) {
+
+                return redirect()->back()->with('status', 'Genre created successfully.');
+            }
+
+            return redirect()->back()->with('errors', new MessageBag(['Something went wrong while adding new genre. Please try again.']));
         }
-
-        return redirect()->back()->with('errors', new MessageBag(['Something went wrong while adding new genre. Please try again.']));
+    	
     }
 }
