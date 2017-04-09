4706688167d8b2e427da87473e939419ae5cee63
GretaM <gretamerkeviciute@gmail.com>
2017-04-09 23:02:36 +0300

    Saving debt price.

--- app/Http/Controllers/OptionController.php
+++ app/Http/Controllers/OptionController.php
@@ -5,8 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
-
 use App\Option;
+use Validator;
+use DB;
 
 class OptionController extends Controller
 {
@@ -15,5 +16,28 @@ class OptionController extends Controller
     	$options = Option::all();
 
     	return view('options.index', compact('options'));
+    }
+
+    public function debtPrice(Request $request)
+    {
+    	$validator =  Validator::make($request->all(), [
+            'price' => 'required|max:255|numeric'
+        ]);
+
+        if ($validator->fails()) {
+
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+
+        $price = Option::create([
+            'name' => 'debt_price',
+            'value' => $request->input('price')
+        ]);
+        if ($price) {
+
+            return redirect('/options')->with('status', 'Genre created successfully.');
+        }
+
+        return redirect()->back()->with('errors', new MessageBag(['Something went wrong while adding new genre. Please try again.']));
     }
 }
